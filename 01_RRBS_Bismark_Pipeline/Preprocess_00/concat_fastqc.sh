21f659e5ee20172f6199635c1951f7422792d911
qitingcai <134165300+qitingcai@users.noreply.github.com>
2025-02-04 00:39:25 +0000

    add fastqc scripts for concat

---
+++ 01_RRBS_Bismark_Pipeline/Preprocess_00/concat_fastqc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --partition=128x24               # Partition/queue to run on
+#SBATCH --time=1-00:00:00                # Max time for job to run
+#SBATCH --job-name=fastqc_concat                # Name for job (shows when running squeue)
+#SBATCH --mail-type=ALL                  # Mail events(NONE,BEGIN,END,FAIL,ALL)
+#SBATCH --mail-user=qcai17@ucsc.edu    # Where to send mail
+#SBATCH --ntasks=1                       # Number of tasks to run
+#SBATCH --cpus-per-task=8                # Number of CPU cores to use per task
+#SBATCH --nodes=1                        # Number of nodes to use
+#SBATCH --mem=10G                        # Ammount of RAM to allocate for the task
+#SBATCH --output=slurm_%j.out            # Standard output and error log
+#SBATCH --error=slurm_%j.err             # Standard output and error log
+#SBATCH --no-requeue                     # don't requeue the job upon NODE_FAIL
+
+### loading fastqc, contains FastQC v0.12.1 ###
+module load parallel fastqc
+
+### running fastqc on trimmed, concatenated reads ###
+fastqc -o fastqc/ --noextract /hb/groups/kelley_lab/tina/mytilus/02_trim/final_trim0603/concat/*.fq.gz -t 8
