03d900e70142938501fa41a7a958952004190166
qitingcai <134165300+qitingcai@users.noreply.github.com>
2025-02-04 00:35:19 +0000

    adding preprocessing code

---
+++ 01_RRBS_Bismark_Pipeline/Preprocess_00/concat_lanes.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#SBATCH --partition=128x24               # Partition/queue to run on
+#SBATCH --time=3-00:00:00                # Max time for job to run
+#SBATCH --job-name=concat                # Name for job (shows when running squeue)
+#SBATCH --mail-type=ALL                  # Mail events (NONE,BEGIN,END,FAIL,ALL)
+#SBATCH --mail-user=qcai17@ucsc.edu      # Where to send mail
+#SBATCH --ntasks=1                       # Number of tasks to run
+#SBATCH --cpus-per-task=20               # Number of CPU cores to use per task
+#SBATCH --nodes=1                        # Number of nodes to use
+#SBATCH --mem=10G                        # Amount of RAM to allocate for the task
+#SBATCH --output=slurm_%j.out            # Standard output and error log
+#SBATCH --error=slurm_%j.err             # Standard output and error log
+#SBATCH --no-requeue                     # Don't requeue the job upon NODE_FAIL
+#SBATCH --array=[1-700]                     # Array job, adjust the range as needed
+
+### Aim of this script: concatenating lane 01-04 for both reverse and forward trimmed reads for all samples ###
+
+# Load the sample name based on the SLURM array task ID
+LINE=$(sed -n "${SLURM_ARRAY_TASK_ID}p" /hb/groups/kelley_lab/tina/mytilus/03_mapping/02_alignment/sample.txt)
+sample=$(echo ${LINE} | awk '{ print $2; }')
+
+# Define the input and output file paths
+input_path="/hb/groups/kelley_lab/tina/mytilus/02_trim/final_trim0603"
+output_path="/hb/groups/kelley_lab/tina/mytilus/02_trim/final_trim0603/concat"
+
+# Ensure the output directory exists
+mkdir -p $output_path
+
+# Concatenate R1 files
+cat ${input_path}/${sample}_L001_R1_001.65bp_5prime.fq.gz \
+    ${input_path}/${sample}_L002_R1_001.65bp_5prime.fq.gz \
+    ${input_path}/${sample}_L003_R1_001.65bp_5prime.fq.gz \
+    ${input_path}/${sample}_L004_R1_001.65bp_5prime.fq.gz > ${output_path}/${sample}_R1_merged.fq.gz
+
+# Concatenate R2 files
+cat ${input_path}/${sample}_L001_R2_001.65bp_5prime.fq.gz \
+    ${input_path}/${sample}_L002_R2_001.65bp_5prime.fq.gz \
+    ${input_path}/${sample}_L003_R2_001.65bp_5prime.fq.gz \
+    ${input_path}/${sample}_L004_R2_001.65bp_5prime.fq.gz > ${output_path}/${sample}_R2_merged.fq.gz
+
+echo "Finished processing sample: $sample"
---
+++ 01_RRBS_Bismark_Pipeline/Preprocess_00/pretrim_multiqc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --partition=128x24               # Partition/queue to run on
+#SBATCH --time=1-00:00:00                # Max time for job to run
+#SBATCH --job-name=multiqc                # Name for job (shows when running squeue)
+#SBATCH --mail-type=ALL                  # Mail events(NONE,BEGIN,END,FAIL,ALL)
+#SBATCH --mail-user=qcai17@ucsc.edu    # Where to send mail
+#SBATCH --ntasks=1                       # Number of tasks to run
+#SBATCH --cpus-per-task=8                # Number of CPU cores to use per task
+#SBATCH --nodes=1                        # Number of nodes to use
+#SBATCH --mem=10G                        # Ammount of RAM to allocate for the task
+#SBATCH --output=slurm_%j.out            # Standard output and error log
+#SBATCH --error=slurm_%j.err             # Standard output and error log
+#SBATCH --no-requeue                     # don't requeue the job upon NODE_FAIL
+
+### Directory that contain all fastqc results on raw RRBS data ###
+cd /hb/groups/kelley_lab/tina/mytilus/01_fastqc
+
+### loading multiqc, contains multiqc, version 1.25.2 ###
+module load miniconda3
+conda activate multiqc
+
+### running multiqc on all fastqc results to create aggregated results for easier viewing ###
+multiqc -v -o multiqc/ /hb/groups/kelley_lab/tina/mytilus/01_fastqc/results/
