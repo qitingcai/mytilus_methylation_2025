ad5b257080646ab769cbcfe07c23685d44e0c92f
qitingcai <134165300+qitingcai@users.noreply.github.com>
2025-03-26 22:01:54 -0400

    add average methy calculation

---
+++ 01_RRBS_Bismark_Pipeline/00_preprocess/pretrimmed_fastqc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --partition=128x24               # Partition/queue to run on
+#SBATCH --time=1-00:00:00                # Max time for job to run
+#SBATCH --job-name=fastqc                # Name for job (shows when running squeue)
+#SBATCH --mail-type=ALL                  # Mail events(NONE,BEGIN,END,FAIL,ALL)
+#SBATCH --mail-user=qcai17@ucsc.edu    # Where to send mail
+#SBATCH --ntasks=1                       # Number of tasks to run
+#SBATCH --cpus-per-task=8                # Number of CPU cores to use per task
+#SBATCH --nodes=1                        # Number of nodes to use
+#SBATCH --mem=10G                        # Ammount of RAM to allocate for the task
+#SBATCH --output=slurm_%j.out            # Standard output and error log
+#SBATCH --error=slurm_%j.err             # Standard output and error log
+#SBATCH --no-requeue                     # don't requeue the job upon NODE_FAIL
+
+### Loading module fastqc, -> contains FastQC v0.12.1 ###
+module load fastqc
+
+### Running fastqc on all raw RRBS data to assess quality ###
+fastqc -o results/ --noextract /hb/groups/kelley_lab/tina/mytilus/data/*.fastq.gz -t 8
